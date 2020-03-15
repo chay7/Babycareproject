749601992117955f85796e5f70cad4469ebd6f19
chaitanyapc95 <chaitanyapenumuchupc@gmail.com>
2020-03-14 20:20:06 -0400

    version 13

---
+++ app/src/main/java/com/example/babycareproject/activity/activity/activity/AdminDashBoardActivity.java
@@ -0,0 +1,84 @@
+package com.example.babycareproject.activity.activity.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+
+import com.example.babycareproject.R;
+
+public class AdminDashBoardActivity extends AppCompatActivity {
+    CardView cd_schedule,cd_profile;
+    Button btn_logout,btn_rating;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admindashboard);
+        getSupportActionBar().setTitle("Nanies/Nurse DashBoard");
+        cd_schedule=(CardView)findViewById(R.id.cd_schedule);
+        cd_schedule.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,NanniNurseScheduleActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        cd_profile=(CardView)findViewById(R.id.cd_profile);
+        cd_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,AdminEditProfileActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        btn_logout=(Button)findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,MainActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+        btn_rating=(Button)findViewById(R.id.btn_rating);
+        btn_rating.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(AdminDashBoardActivity.this,CareTackersReviewRatingActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);//Menu Resource, Menu
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.idlogout:
+                // Toast.makeText(getApplicationContext(),"logout Selected",Toast.LENGTH_LONG).show();
+                Intent i=new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(i);
+                finish();
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
